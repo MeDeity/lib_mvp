ca7bb8b7ea94730eb23ad2370194d20e22681c65
fwenhua <langrenbule@gmail.com>
2019-09-09 11:05:20 +0800

    优化 日常优化

--- lib/lib_mvp.dart
+++ lib/lib_mvp.dart
@@ -1,7 +1,14 @@
 library lib_mvp;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:lib_mvp/mvp/base_page_presenter.dart';
+export 'package:lib_mvp/mvp/base_page_state.dart';
+export 'package:lib_mvp/mvp/contract.dart';
+export 'package:lib_mvp/network/base_entity.dart';
+export 'package:lib_mvp/network/dio_utils.dart';
+export 'package:lib_mvp/network/error_handler.dart';
+export 'package:lib_mvp/res/resources.dart';
+export 'package:lib_mvp/router/app_navigator.dart';
+export 'package:lib_mvp/utils/toast_utils.dart';
+export 'package:lib_mvp/utils/utils.dart';
+export 'package:lib_mvp/widget/base_dialog.dart';
+export 'package:lib_mvp/widget/loading_dialog.dart';
